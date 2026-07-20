0114948764ffcf22bd92e28010d988ac45e201c2
GPA <GPA>
2026-07-20 16:53:29 -0600

    intento de fix

--- quotes/urls.py
+++ quotes/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     
     path("", views.quote, name="quote"), 
-    path("<int:day>", views.days_week_with_number), 
-    path("<str:day>", views.days_week, name="day-quote"), 
+    # path("<int:day>", views.days_week_with_number), 
+    # path("<str:day>", views.days_week, name="day-quote"), 
+    path("detail/", views.detail,name="detail"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -3,16 +3,6 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 
-days_of_week = {
-    "monday": "Pienso, luego existo",
-    "tuesday": "La vida es unn sueño",
-    "wednesday": "El conocimiento es poder",
-    "thursday": "Sé el cambio que quieres ver en el mundo",
-    "friday": "Solo sé que no sé nada",
-    "saturday": "Vive como si fuera el último día",
-    "sunday": "Da un poco más todos los días"   
-}
-
 
 def quote(request):
     quote_day = [
@@ -34,31 +24,34 @@ def quote(request):
     return render(request, "quotes/landing.html", {
         "quote_day": quote_day
     })
+    
+def detail(request,day):
+    HttpResponse(f'Frase del día {day}')
 
-def days_week(request, day):
-    try:
-        quote_text = days_of_week[day]
-        return HttpResponse(quote_text)
-    except KeyError:
-        return HttpResponseNotFound("No hay frase para este día")
+# def days_week(request, day):
+#     try:
+#         quote_text = days_of_week[day]
+#         return HttpResponse(quote_text)
+#     except KeyError:
+#         return HttpResponseNotFound("No hay frase para este día")
     
     
-def index(request):
-    list_items = ""
-    days = list(days_of_week.keys())
+# def index(request):
+#     list_items = ""
+#     days = list(days_of_week.keys())
     
-    for day in days:
-        day_path = reverse("day-quote", args=[day])
-        list_items += f"<li><a href='{day_path}'>{day}</a></li>"
+#     for day in days:
+#         day_path = reverse("day-quote", args=[day])
+#         list_items += f"<li><a href='{day_path}'>{day}</a></li>"
         
-    response_html = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_html)
+#     response_html = f"<ul>{list_items}</ul>"
+#     return HttpResponse(response_html)
 
 
-def days_week_with_number(request, day):
-    days = list(days_of_week.keys())
-    if day > len(days):
-        return HttpResponseNotFound("<h1>El día no existe</h1>")
-    redirect_day = days[day-1]
-    redirect_path = reverse("day-quote",args=[redirect_day])
-    return HttpResponseRedirect(redirect_path)
+# def days_week_with_number(request, day):
+#     days = list(days_of_week.keys())
+#     if day > len(days):
+#         return HttpResponseNotFound("<h1>El día no existe</h1>")
+#     redirect_day = days[day-1]
+#     redirect_path = reverse("day-quote",args=[redirect_day])
+#     return HttpResponseRedirect(redirect_path)
