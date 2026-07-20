7214127d8ac0b92f53ef0f4828e767205401ef46
GPA <GPA>
2026-07-20 12:49:49 -0600

    Tagv2 - urltag

--- landing/views.py
+++ landing/views.py
@@ -5,10 +5,22 @@ from datetime import date
 
 def home(request):
     today = date.today()
-    stack = ['Python','Django','Golang','PHP','JS']
+    stack = [{'id': 'python',
+              'name':'Python'},
+             {'id': 'django',
+              'name':'Django'},
+             {'id': 'golang',
+              'name':'Golang'},
+             {'id': 'php',
+              'name':'PHP'},
+             {'id': 'js',
+              'name':'JS'}]
     return render(request, "landing/landing.html", {
         "name": "Fernando",
         "today": today,
         "age": 24,
         "stack": stack
     })
+    
+def stack_detail(request, tool):
+    return HttpResponse(f"Tecnología: {tool}")
